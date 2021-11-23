cc01d0b572b550780a162023282e949f8fbc5a57
Evan Daniel <Evan.Daniel@sckans.edu>
2021-11-23 12:03:27 -0600

    edit

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import Picture from './kangaroo.jpeg'; 
+import Logo from './logo.svg'; 
+
+const Hero = () => {
+    return (
+        <>
+            <img src={Picture} />
+        </>
+    ); 
+}; 
+
+export default Hero; 
